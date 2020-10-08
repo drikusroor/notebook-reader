2356dec9003960b1b760b80a308a57e4d5676c6a
Drikus Roor <drikusroor@gmail.com>
2020-10-08 21:45:18 +0200

    Use boxes to make image crops of words
    TODO:
    Make crops larger as words sometimes do not fit inside the box

--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 from scene_text import AllWordsRecognizer
 
-img = cv2.imread('images/test.png')
+img = cv2.imread("images/test.png")
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # threshold image
 ret, thresh = cv2.threshold(imgray, 150, 255, 0)
-cv2.imwrite('thresh.jpg', thresh)
+cv2.imwrite("images/thresh.jpg", thresh)
 
 # edge contours
 contours, hierarchy = cv2.findContours(thresh, 1, 2)
@@ -15,14 +15,46 @@ contours, hierarchy = cv2.findContours(thresh, 1, 2)
 pipeline = AllWordsRecognizer()
 
 # read an image
-img = cv2.imread('thresh.jpg')
+img = cv2.imread("images/thresh.jpg")
 
 # detect and recognize all words in the image
 words, boxes = pipeline.get_all_words(img)
 
+counter = 1
+
 # loop over the bounding boxes
 for box in boxes:
-	# TODO
-	# Figure out how to use boxes to make crops of words
-	# that I can use to apply text recognition on
-	print(box)
+    
+		height, width = np.shape(img)
+
+    X, y = box[:, 0], box[:, -1]
+
+    minX = int(np.min(X))
+    maxX = int(np.max(X))
+    minY = int(np.min(y))
+    maxY = int(np.max(y))
+
+		if minX > 5:
+			minX -= 5
+		else:
+			minX = 0
+
+		if minY > 5:
+			minY -= 5
+		else:
+			minY = 0
+
+		if maxX <= width - 5:
+			maxX +=5
+		else
+			maxX = width
+
+		if maxY <= height - 5:
+			maxY += 5
+		else:
+			maxY = height
+
+    crop_img = img[minY:maxY, minX:maxX]
+    cv2.imwrite("boxes/crop-{0}.jpg".format(counter), crop_img)
+
+    counter += 1
